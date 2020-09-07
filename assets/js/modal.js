a6b27ee8a85f081cddf6c3d236788343cacb1923
Shann Aurelle Ripalda <shannaurelleg@gmail.com>
2020-09-07 14:56:37 +0800

    made modal targeting correct

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,8 +1,13 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+function clearImageOutput(){
+    console.log("Clear Image!");
+    var image = document.getElementById('output');
+    image.src = "assets/img/no-image.png";
+}
+
+var modals = document.getElementsByClassName('modal');
 
 // Get the button that opens the modal
-var btn = document.getElementsByClassName("modal-link");
+var btn = document.getElementsByClassName("link");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -10,19 +15,26 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal
 for(let i = 0; i < btn.length; i++){
     btn[i].onclick = function(event) {
-    event.preventDefault();
-    modal.style.display = "block";
+    let modal_id = btn[i].getAttribute('data-id');
+    let target_modal = document.getElementById(modal_id);
+    target_modal.style.display = "block";
     }
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-modal.style.display = "none";
+    for(let i = 0; i < modals.length; i++){
+        modals[i].style.display = "none";
+        clearImageOutput();
+    }
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-if (event.target == modal) {
-    modal.style.display = "none";
-}
+    for(let i = 0; i < modals.length; i++){
+        if (event.target == modals[i]) { 
+            modals[i].style.display = "none"; 
+            clearImageOutput();
+        }
+    }
 }
